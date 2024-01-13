0564cd30d8ee1cd08f6744325ef9ba93174ef52e
IlyaZhGIT <ilyazh0709@gmail.com>
2024-01-13 23:32:34 +0300

    S_K

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-kpa)e!o7vw^04a#hxop$6+$9t5nf_6w1hd8^0d&$&h=(3vl&yz"
+# SECRET_KEY = "django-insecure-kpa)e!o7vw^04a#hxop$6+$9t5nf_6w1hd8^0d&$&h=(3vl&yz"
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "myapp",
+    "хуйапп",
 ]
 
 MIDDLEWARE = [
