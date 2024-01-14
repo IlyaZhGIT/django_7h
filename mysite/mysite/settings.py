206d224db36b17b5c4b475057d385a19f32b2fed
IlyaZhGIT <ilyazh0709@gmail.com>
2024-01-14 20:39:15 +0300

    navbar

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-kpa)e!o7vw^04a#hxop$6+$9t5nf_6w1hd8^0d&$&h=(3vl&yz"
+SECRET_KEY = "django-insecure-kpa)e!o7vw^04a#hxop$6+$9t5nf_6w1hd8^0d&$&h=(3vl&yz"
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+# SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "myapp",
-    "хуйапп",
 ]
 
 MIDDLEWARE = [
@@ -120,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
