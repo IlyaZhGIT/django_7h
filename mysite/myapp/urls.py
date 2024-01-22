4f283214f3f9728a8ed34e75bbe62aae3ce99c7e
IlyaZhirov <ilyazh0709@gmail.com>
2024-01-22 15:35:46 +0300

    del and update

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -5,6 +5,9 @@ from myapp.views import *
 app_name = "myapp"
 
 urlpatterns = [
-    path("products/", products),
+    path("products/", products, name="products"),
     path("products/<int:my_id>/", product_id, name="details"),
+    path("additem/", additem, name="additem.html"),
+    path("updateitem/<int:my_id>/", updateitem, name="updateitem"),
+    path("deleteitem/<int:my_id>/", deleteitem, name="deleteitem"),
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from importlib.resources import contents
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import Product
@@ -8,10 +9,43 @@ from .models import Product
 def products(request):
     items = Product.objects.all()
     context = {"items": items}
-    return render(request, "myapp/product.html", context)
+    return render(request, "myapp/products.html", context)
 
 
 def product_id(request, my_id):
     item = Product.objects.get(id=my_id)
     context = {"item": item}
     return render(request, "myapp/detail.html", context=context)
+
+
+def additem(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        description = request.POST.get("description")
+        image = request.FILES["upload"]
+        item = Product(name=name, price=price, description=description, image=image)
+        item.save()
+    return render(request, "myapp/additem.html")
+
+
+def updateitem(request, my_id):
+    item = Product.objects.get(id=my_id)
+    if request.method == "POST":
+        item.name = request.POST.get("name")
+        item.price = request.POST.get("price")
+        item.description = request.POST.get("description")
+        item.image = request.FILES.get("upload", item.image)
+        item.save()
+        return redirect("/myapp/products")
+    context = {"item": item}
+    return render(request, "myapp/updateitem.html", context=context)
+
+
+def deleteitem(request, my_id):
+    item = Product.objects.get(id=my_id)
+    if request.method == "POST":
+        item.delete()
+        return redirect("/myapp/products")
+    context = {"item": item}
+    return render(request, "myapp/deleteitem.html", context=context)
