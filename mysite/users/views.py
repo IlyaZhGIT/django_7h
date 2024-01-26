d2d8248e56b9d23a7ec0e988aebca2d315266b71
IlyaZhirov <ilyazh0709@gmail.com>
2024-01-26 09:15:04 +0300

    +

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import NewUserForms
 
 # Create your views here.
+
+
+def register(request):
+    if request.method == "POST":
+        form = NewUserForms(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("myapp:products")
+    form = NewUserForms()
+    context = {"form": form}
+    return render(request, "user/register.html", context=context)
