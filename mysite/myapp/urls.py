f83e12328d34701f23c67a921185675891177b76
IlyaZhGIT <ilyazh0709@gmail.com>
2024-01-27 12:32:27 +0300

    Add class view and razebav nahui _tkinter

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -5,8 +5,10 @@ from myapp.views import *
 app_name = "myapp"
 
 urlpatterns = [
-    path("products/", products, name="products"),
-    path("products/<int:my_id>/", product_id, name="details"),
+    # path("products/", products, name="products"),
+    path("products/", ProductListView.as_view(), name="products"),
+    # path("products/<int:my_id>/", product_id, name="details"),
+    path("products/<int:pk>/", ProductDetailView.as_view(), name="details"),
     path("additem/", additem, name="additem"),
     path("updateitem/<int:my_id>/", updateitem, name="updateitem"),
     path("deleteitem/<int:my_id>/", deleteitem, name="deleteitem"),
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,23 +1,37 @@
 from importlib.resources import contents
+from tkinter import image_names
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, DetailView
 
 
 from .models import Product
 
 
 # Create your views here.
-def products(request):
-    items = Product.objects.all()
-    context = {"items": items}
-    return render(request, "myapp/products.html", context)
+# def products(request):
+#     items = Product.objects.all()
+#     context = {"items": items}
+#     return render(request, "myapp/products.html", context)
 
 
-def product_id(request, my_id):
-    item = Product.objects.get(id=my_id)
-    context = {"item": item}
-    return render(request, "myapp/detail.html", context=context)
+class ProductListView(ListView):
+    model = Product
+    template_name = "myapp/products.html"
+    context_object_name = "items"
+
+
+# def product_id(request, my_id):
+#     item = Product.objects.get(id=my_id)
+#     context = {"item": item}
+#     return render(request, "myapp/detail.html", context=context)
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "myapp/detail.html"
+    context_object_name = "item"
 
 
 @login_required
